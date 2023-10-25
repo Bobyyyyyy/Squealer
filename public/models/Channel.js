d3d1ad481e06512d3014280e2250ffc233e46752
Tiestaa <francesco.testa3@studio.unibo.it>
2023-10-25 23:02:50 +0200

    add Channel.js

--- public/models/Channel.js
+++ public/models/Channel.js
@@ -5,10 +5,54 @@ const ChannelSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+
+    description: {
+        type: String,
+    },
+
+    /*
     public: {
         type: Boolean,
         required: true,
     },
+    PER ORA TUTTI PUBLIC
+     */
+
+    posts: [
+        {
+            type: String,       //ID del post
+            required: true
+        }
+    ],
+
+    /* primo proprietario */
+    creator: {
+      type: String,
+      required: true,
+    },
+
+    /* Altri utenti che sono proprietari */
+    /* Possono aggiungere utenti agli scrittori/accettare nei canali privati */
+
+    ownership: [
+        {
+            type:String,
+            required: true,
+        }
+    ],
+
+    followers:[
+        {
+            user: {
+                type: String,
+                required: true,
+            },
+            canWrite: {
+                type: Boolean,
+                required: true,
+            }
+        }
+    ]
 })
 
 const Channel = mongoose.model("Channel", ChannelSchema);
