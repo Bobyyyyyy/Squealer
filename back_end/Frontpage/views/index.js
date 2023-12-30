b0068916974dd6470b009f00478f882f755e3fca
Alessandro <alex.tomaiuolo22@gmail.com>
2023-12-30 22:49:38 +0100

    login page

--- back_end/Frontpage/views/index.js
+++ back_end/Frontpage/views/index.js
@@ -5,16 +5,11 @@ $('#login-form').on('submit', event => {
         type: 'post',
         data: {user: $('input[name="user"]').val(), password: $('input[name="password"]').val()},
         success: () => {
-            alert("RELOAD");
-            location.reload();}
+            location.reload();
+        }
     })
     .fail((error) => {
-        if(error.responseText === 'password errata'){
-            $('input[name="password"]').after('<span class="link" style="color: #ea0000 !important;">password errata!</span>');
-        }
-        else{
-            $('input[name="user"]').after('<span class="link" style="color: #ea0000 !important;">utente errato!</span>');
-        }
+        $('#error').empty().html(error.responseText);
     })
 });
 
