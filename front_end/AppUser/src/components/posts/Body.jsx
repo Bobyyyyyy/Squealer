e5116e8cd0fad005176ce28fcb55a3349d53a522
AleBombi <118299985+AleBombi@users.noreply.github.com>
2023-12-30 13:46:29 +0100

    feature: inserimento link e @utenti nel testo

--- front_end/AppUser/src/components/posts/Body.jsx
+++ front_end/AppUser/src/components/posts/Body.jsx
@@ -1,38 +1,63 @@
 import MappaPost from "./maps/MappaPost.jsx";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 function Body({post}) {
 
     const URLHTTPREGEX = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g
+    const ACCOUNTREGEX = /\@\w+/g
+    console.log(post.content.match(ACCOUNTREGEX))
 
-    const parseContentText = (links) => {
-        let content_noLink = '';
-        links.forEach(link => {content_noLink = post.content.replace(link,'__SPLIT__').split('__SPLIT__')});
-        let innerContent = [];
+    const parseContentText = (content, linkRegex, userRegex) => {
+        // Initialize arrays to store links and users
+        const links = content.match(linkRegex) || [];
+        const users = content.match(userRegex) || [];
 
-        for (let i = 0; i < content_noLink.length; i++){
-            let newLinkComponent = [];
-            if(typeof links[i] !== 'undefined') {
-                newLinkComponent.push(<a key={i} className="text-blue-500" href={links[i]} target="_blank">{links[i]}</a>)
-                innerContent.push(<p key={i} className="text-base">{content_noLink[i]}{newLinkComponent}</p>)
+        // Replace links with a placeholder
+        let modifiedContent = content;
+        links.forEach((link, index) => {
+            modifiedContent = modifiedContent.replace(link, `__LINK_${index}__`);
+        });
+
+        // Replace users with a placeholder
+        users.forEach((user, index) => {
+            modifiedContent = modifiedContent.replace(user, `__USER_${index}__`);
+        });
+
+        // Split the modified content based on the placeholders
+        const contentArray = modifiedContent.split(/(__LINK_\d+__|__USER_\d+__)/).map((item, index) => {
+            if (item.startsWith('__LINK_')) {
+                const linkIndex = parseInt(item.replace('__LINK_', ''), 10);
+                return (
+                    <React.Fragment key={index}>
+                        <a className="text-blue-500" href={links[linkIndex]} target="_blank">
+                            {links[linkIndex]}
+                        </a>
+                    </React.Fragment>
+                );
+            } else if (item.startsWith('__USER_')) {
+                const userIndex = parseInt(item.replace('__USER_', ''), 10);
+                return (
+                    <React.Fragment key={index}>
+                        <Link className="text-blue-500" to={`/search/${users[userIndex].slice(1)}`}>
+                            {users[userIndex]}
+                        </Link>
+                    </React.Fragment>
+                );
             }
-        }
-        return innerContent;
-    }
+            return <React.Fragment key={index}>{item}</React.Fragment>;
+        });
+
+        return <p className="text-base">{contentArray}</p>;
+    };
 
 
     return (
         <div className={"flex justify-center md:max-h-[28rem]"}>
             {post.contentType === "text" &&
-                <>
-                    {post.content.match(URLHTTPREGEX) ? (
-                        <div className = "p-2 overflow-hidden break-words">
-                            {parseContentText(post.content.match(URLHTTPREGEX))}
-                        </div>
-                    ) : (
-                        <p className="p-2 overflow-hidden break-words">{post.content}</p>
-                    )}
-                </>
+                <div className = "px-4 py-2 overflow-hidden break-words">
+                    {parseContentText(post.content, URLHTTPREGEX,ACCOUNTREGEX)}
+                </div>
             }
             {post.contentType === "image" &&
                 <img className="aspect-auto w-full object-cover overflow-hidden" src={post.content} alt={"image"}/>
