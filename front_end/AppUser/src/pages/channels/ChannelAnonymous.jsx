f0fe286e36953067c3ee8165622fe0c46113ed0c
AleBombi <118299985+AleBombi@users.noreply.github.com>
2023-12-31 18:18:41 +0100

    fix: pagina canali anonima

--- front_end/AppUser/src/pages/channels/ChannelAnonymous.jsx
+++ front_end/AppUser/src/pages/channels/ChannelAnonymous.jsx
@@ -72,8 +72,8 @@ function ChannelAnonymous () {
                         {officialChannels!==null && officialChannels.map((channel) => {
                             return (
                                 <Link className="w-full" to={`/channels/${channel.name}`} key={channel._id} >
-                                    <div className="flex w-full justify-start gap-4 border-2 border-black">
-                                        <img src={ProfilePic} alt="immagine canale" className="w-14 h-14"/>
+                                    <div className="flex w-full justify-start gap-4">
+                                        <img src={channel.profilePicture} alt="immagine canale" className="w-14 h-14 object-cover rounded-full"/>
                                         <div className="flex flex-col overflow-x-hidden  mx-2 w-full">
                                             <div className="flex justify-between">
                                                 <span className="font-semibold text-lg">{channel.name}</span>
