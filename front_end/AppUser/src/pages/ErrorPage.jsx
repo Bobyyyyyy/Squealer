019b3f7d63976ae6fe7e42119b3da6695b0798ad
AleBombi <118299985+AleBombi@users.noreply.github.com>
2024-01-06 11:19:06 +0100

    refactor: reso accessibili i file modificati

--- front_end/AppUser/src/pages/ErrorPage.jsx
+++ front_end/AppUser/src/pages/ErrorPage.jsx
@@ -6,10 +6,11 @@ function ErrorPage() {
     return (
         <div className="flex flex-col w-full h-full justify-start items-center gap-8 mt-8">
             <h2 className="text-4xl font-extrabold text-center">ERRORE</h2>
-            <p className="text-2xl font-semibold text-center">{error.message}</p>
+            <p className="text-2xl font-semibold text-center" aria-label={`Errore: ${error.message}`}>{error.message}</p>
             <Link
                 className="button uppercase"
                 to="/"
+                aria-label="Torna alla homepage"
             >
                 Torna alla Homepage
             </Link>
