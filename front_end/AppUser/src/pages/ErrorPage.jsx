6bfb96d5c572be5887d684f64a7f45e0edf7b7ed
AleBombi <118299985+AleBombi@users.noreply.github.com>
2024-01-07 01:42:42 +0100

    style: abbellimento pagina errore e not trovata

--- front_end/AppUser/src/pages/ErrorPage.jsx
+++ front_end/AppUser/src/pages/ErrorPage.jsx
@@ -4,11 +4,11 @@ function ErrorPage() {
     const error = useRouteError()
 
     return (
-        <div className="flex flex-col w-full h-full justify-start items-center gap-8 mt-8">
+        <div className="flex flex-col w-full h-full justify-start items-center gap-8 mt-8 px-4">
             <h2 className="text-4xl font-extrabold text-center">ERRORE</h2>
             <p className="text-2xl font-semibold text-center" aria-label={`Errore: ${error.message}`}>{error.message}</p>
             <Link
-                className="button uppercase"
+                className="button uppercase text-center"
                 to="/"
                 aria-label="Torna alla homepage"
             >
